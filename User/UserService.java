380adab8a64cc18885043bd814a179b3ceb77df1
Mesoma10 <Tzmish10@gmail.com>
2023-07-13 15:11:40 -0400

    made userFileDAO property final

--- User/UserService.java
+++ User/UserService.java
@@ -1,18 +1,12 @@
 package User;
 
-import utils.FileChecker;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.UUID;
 
 //Service class for User
 public class UserService {
 
-    private UserFileDAO userFileDAO;
+    private final UserFileDAO userFileDAO;
     public UserService(UserFileDAO userFileDAO) {
         this.userFileDAO = userFileDAO;
     }
@@ -38,14 +32,5 @@ public class UserService {
         }catch(NullPointerException e){
             throw new NullPointerException();
         }
-//        //check if the user exists and return them otherwise we return the default
-//        for (User user:users){
-//            if (user.getUserId().equals(userId)){
-//                expectedUser = user;
-//                return expectedUser;
-//            }
-//        }
-//        
-//        return expectedUser;
     }
 }
