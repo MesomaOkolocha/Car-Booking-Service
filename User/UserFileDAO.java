caa4075d48e2b4b1f47753208cf0d1e825263552
Mesoma10 <Tzmish10@gmail.com>
2023-07-13 15:17:16 -0400

    Updated get Users Method to read file after time method is called

--- User/UserFileDAO.java
+++ User/UserFileDAO.java
@@ -3,7 +3,6 @@ package User;
 import utils.FileChecker;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,30 +11,22 @@ import java.util.UUID;
 
 //Data access class for users, we are going to store users using an array
 public class UserFileDAO implements UserDaoInterface {
-    private static final File file = FileChecker.createFile("/Users/mesomaokolocha/Desktop/CLI_Project/src/users.csv");
-    private static List<User> users;
+    private static final File FILE = FileChecker.createFile("/Users/mesomaokolocha/Desktop/CLI_Project/src/users.csv");
 
-    static {
-        users = new ArrayList<>();
-        loadUsersFromFile(); // Call the method to load users from the file during initialization
-    }
-
-    private static void loadUsersFromFile() {
+    //retrieve users from the database
+    @Override
+    public List<User> getUsers(){
+        List<User> users = new ArrayList<>();
         try {
-            Scanner scanner = new Scanner(file);
+            Scanner scanner = new Scanner(FILE);
             while (scanner.hasNext()) {
                 String[] data = scanner.nextLine().split(",");
                 users.add(new User(UUID.fromString(data[0]), data[1]));
             }
             scanner.close(); // Close the scanner after reading the file
+            return users;
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
-    }
-
-    //retrieve users from the database
-    @Override
-    public List<User> getUsers(){
-        return users;
     }
 }
